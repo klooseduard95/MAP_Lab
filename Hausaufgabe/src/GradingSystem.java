8297d36f47a43cba17dce94e012121303035e842
klooseduard95 <eduard.kloos@stud.ubbcluj.ro>
2025-10-18 11:37:48 +0300

    added roundUp function

---
+++ Hausaufgabe/src/GradingSystem.java
@@ -0,0 +1,28 @@
+public class GradingSystem {
+
+    private static int roundUp(int grade) {
+
+        final int MIN_ROUNDING_GRADE = 38;
+        if (grade < MIN_ROUNDING_GRADE) {
+            return grade;
+        }
+
+        int remainder = grade % 5;
+        int nextMultipleOf5;
+
+        if (remainder == 0) {
+            nextMultipleOf5 = grade;
+        } else {
+            nextMultipleOf5 = grade + (5- remainder);
+        }
+
+        int difference = nextMultipleOf5 - grade;
+
+        final int MAX_DIFFERENCE_FOR_ROUNDING = 3;
+        if (difference < MAX_DIFFERENCE_FOR_ROUNDING) {
+            return nextMultipleOf5;
+        } else {
+            return grade;
+        }
+    }
+}
