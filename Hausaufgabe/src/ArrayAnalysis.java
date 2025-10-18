a315e3755bce4c2c08424f6aefcf4a0e9dd279dd
klooseduard95 <eduard.kloos@stud.ubbcluj.ro>
2025-10-18 12:34:12 +0300

    added method findMaxSumOfNMinusOne

--- Hausaufgabe/src/ArrayAnalysis.java
+++ Hausaufgabe/src/ArrayAnalysis.java
@@ -32,4 +32,22 @@ public class ArrayAnalysis {
         return minNumber;
     }
 
+    public static long findMaxSumOfNMinusOne(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            return 0;
+        }
+
+        long totalSum = 0;
+        int minimum = numbers[0];
+
+        for (int number : numbers) {
+            totalSum += number;
+            if (number < minimum) {
+                minimum = number;
+            }
+        }
+
+        return totalSum - minimum;
+    }
+
 }
