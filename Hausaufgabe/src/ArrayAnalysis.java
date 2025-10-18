3569a0dad5a8e269a84b9c3a5ae26a69649643e5
klooseduard95 <eduard.kloos@stud.ubbcluj.ro>
2025-10-18 12:28:42 +0300

    added method findMinimum

--- Hausaufgabe/src/ArrayAnalysis.java
+++ Hausaufgabe/src/ArrayAnalysis.java
@@ -16,4 +16,20 @@ public class ArrayAnalysis {
         return maxNumber;
     }
 
+    public static int findMinimum(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            return 0;
+        }
+
+        int minNumber = numbers[0];
+
+        for (int number : numbers) {
+            if (number < minNumber) {
+                minNumber = number;
+            }
+        }
+
+        return minNumber;
+    }
+
 }
