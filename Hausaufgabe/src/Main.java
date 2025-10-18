665be9973cc588c7f4f1d2d205eed97880fd96fc
klooseduard95 <eduard.kloos@stud.ubbcluj.ro>
2025-10-18 12:42:09 +0300

    Added exampleArray to main and called each method

--- Hausaufgabe/src/Main.java
+++ Hausaufgabe/src/Main.java
@@ -23,5 +23,27 @@ public class Main {
         // 4. Maximale abgerundete Note (Max Rounded Grade)
         int maxRoundedGrade = GradingSystem.getMaxRoundedGrade(sampleGrades);
         System.out.println("4. Maximale abgerundete Note: " + maxRoundedGrade);
+
+
+        int[] exampleArray = {4, 8, 3, 10, 17};
+
+        System.out.println("Eingabe Array (Input Array): " + Arrays.toString(exampleArray));
+        System.out.println("----------------------------------------");
+
+        // 1. Maximale Zahl
+        int maxNumber = ArrayAnalysis.findMaximum(exampleArray);
+        System.out.println("1. Maximale Zahl: " + maxNumber);
+
+        // 2. Minimale Zahl
+        int minNumber = ArrayAnalysis.findMinimum(exampleArray);
+        System.out.println("2. Minimale Zahl: " + minNumber);
+
+        // 3. Maximale Summe von n-1 Zahlen
+        long maxSum = ArrayAnalysis.findMaxSumOfNMinusOne(exampleArray);
+        System.out.println("3. Maximale Summe von n-1 Zahlen: " + maxSum);
+
+        // 4. Minimale Summe von n-1 Zahlen
+        long minSum = ArrayAnalysis.findMinSumOfNMinusOne(exampleArray);
+        System.out.println("4. Minimale Summe von n-1 Zahlen: " + minSum);
     }
 }
